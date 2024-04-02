10d6fbe3f76d0f54e66c075d4890394e04c16cf2
jphnkaptain <johnkaptain@hotmail.com>
2024-04-02 17:35:30 +0300

    Update sketch_copy_20240402170442.ino

--- sketch_copy_20240402170442/sketch_copy_20240402170442.ino
+++ sketch_copy_20240402170442/sketch_copy_20240402170442.ino
@@ -1,9 +1,50 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <MPU6050.h>
+#include <Servo.h>
 
+Servo servox;
+Servo servoy;
+
+const int servox_pin = 3;
+const int servoy_pin = 4;
+
+
+MPU6050 sensor;
+
+
+void setup() {
+  servox.attach(servox_pin);
+  servoy.attach(servoy_pin);
+
+  
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+  
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("Initializing the sensor");
+  
+  sensor.initialize();
+  Serial.println(sensor.testConnection() ? "Successfully Connected" : "Connection failed");
+
+  delay(1000);
+  Serial.println("Taking Values from the sensor");
+  delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensor.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
+  int servox_thesi = map(gx, 20000, -20000, 0, 180);
+  int servoy_thesi = map(gy, 20000, -20000, 0, 180);
+
+  Serial.print("Gyro X: ");
+  Serial.println(gx);
+  Serial.print("Gyro Y: ");
+  Serial.println(gy);
+
+  servox.write(servox_thesi);
+  servoy.write(servoy_thesi);
+
+  delay(100);
 }
